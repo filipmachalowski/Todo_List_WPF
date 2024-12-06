521da068d077d2f10256c5a226015d8296a267b8
filipmachalowski <143288486+filipmachalowski@users.noreply.github.com>
2024-12-06 16:25:05 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Todo_List_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
